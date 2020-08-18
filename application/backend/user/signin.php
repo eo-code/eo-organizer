4876f9a19f1887a7e2d7f0abeb7782acd78d4728
fikihamar <fikihamar05@gmail.com>
2020-08-18 16:35:47 +0700

    membuat sistem signin

---
+++ application/backend/user/signin.php
@@ -0,0 +1,8 @@
+<?php
+
+// Variable dari form user
+$username = $_POST["username"];
+$password = $_POST["password"];
+$name = $_POST["password"];
+$alamat = $_POST["alamat"];
+$no_hp = $_POST["no_hp"];
